977426faa17dde0c231400e6222accd812932f22
goongoguma <wogus7an@gmail.com>
2018-10-08 12:37:02 +0900

    snake-game

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -5,33 +5,37 @@ import {ROWS, COLS} from './config';
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [
-    {x: 2, y: 0},
-    {x: 1, y: 0},
-    {x: 0, y: 0},
-  ];
-
+  this.joints = [{x: 3, y: 0},{x: 2, y: 0},{x: 1, y: 0},{x: 0, y: 0}];
   // 먹이의 좌표
   this.fruit = {x: 3, y: 5};
+  this.counterSwitch = 'right';
 }
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
+  this.counterSwitch = 'up';
+  
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
+  this.counterSwitch = 'down';
+  
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
+  this.counterSwitch = 'left';
+  
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
+  this.counterSwitch = 'right';
+  
   console.log('right');
 }
 
@@ -40,6 +44,28 @@ SnakeGameLogic.prototype.nextState = function() {
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
+  
+  // 기본 움직임
+    if(this.counterSwitch === 'up') {
+    this.joints.pop();
+    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y-1});
+  } else if(this.counterSwitch === 'down') {
+    this.joints.pop();
+    this.joints.unshift({x: this.joints[0].x, y: this.joints[0].y+1});
+  } else if(this.counterSwitch === 'left') {
+    this.joints.pop();
+    this.joints.unshift({x: this.joints[0].x-1, y: this.joints[0].y});
+  } else if(this.counterSwitch === 'right') {
+    this.joints.pop();
+    this.joints.unshift({x: this.joints[0].x+1, y: this.joints[0].y});
+  }
+
+  // 과일 먹으면 하나 늘어남 
+  if(this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y) {
+    this.joints.push({ x: this.joints[0].x, y: this.joints[0].y})
+  }
+
+  // if(this.joints[0].x === ROWS || this.joints[0].y === 20)
   return true;
 }
 
