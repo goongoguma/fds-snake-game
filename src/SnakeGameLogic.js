495650a482630e1fbad420aff7c1b83090bfe494
goongoguma <wogus7an@gmail.com>
2018-10-08 14:21:24 +0900

    Baam-Game

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {ROWS, COLS} from './config';
 
 // NOTE: ROWS, COLS에는 행의 개수, 열의 개수가 저장되어 있습니다.
@@ -15,28 +14,24 @@ function SnakeGameLogic() {
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
   this.counterSwitch = 'up';
-  
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   this.counterSwitch = 'down';
-  
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   this.counterSwitch = 'left';
-  
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   this.counterSwitch = 'right';
-  
   console.log('right');
 }
 
@@ -61,13 +56,26 @@ SnakeGameLogic.prototype.nextState = function() {
     this.joints.unshift({x: this.joints[0].x+1, y: this.joints[0].y});
   }
 
-  // 과일 먹으면 하나 늘어남 
+  // 과일 먹으면 하나 늘어남, 과일 랜덤좌표로 이동
   if(this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y) {
     this.joints.push({ x: this.joints[0].x, y: this.joints[0].y})
+    this.fruit = {x: this.fruit.x = Math.floor(Math.random()*COLS), y: this.fruit.y = Math.floor(Math.random()*ROWS)}
+    console.log(this.fruit)
+  }
+  
+  // 게임 끝 
+  if(this.joints[0].x >= COLS || this.joints[0].y >= ROWS || this.joints[0].x <= -1 || this.joints[0].y <= -1) {
+    return false
+  }
+  // 게임 끝 (꼬리겹침)
+  for(let i = 1; i < this.joints.length-1; i++) {
+    if(this.joints[0].x === this.joints[i].x && this.joints[0].y === this.joints[i].y) {
+      return false
+    }
   }
 
-  // if(this.joints[0].x === ROWS || this.joints[0].y === 20)
   return true;
 }
 
 export default SnakeGameLogic;
+
